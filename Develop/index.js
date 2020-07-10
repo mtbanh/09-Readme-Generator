8c231dfbdce33c7ed4079181d417f4e27e5774b7
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-10 14:44:59 -0700

    created readme markdown

--- Develop/index.js
+++ Develop/index.js
@@ -15,66 +15,87 @@
 //need to access the userinput and display it into the html pg
 var fs = require("fs");
 var inquirer = require("inquirer");
-var axios = require("axios")
+// var axios = require("axios");
+const util = require("util");
 
-inquirer
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser(){
+return inquirer
     .prompt([
         {
+            type: "input",
             message: "What is your github username?",
             name: "username"
         },
         {
+            type: "input",
             message: "What is the name of your project?",
             name: "projectName"
         },
         {
+            type: "input",
             message: "Write a brief description of the project?",
             name: "projectDescription"
         },
         {
+            type: "input",
             message: "What are some tools and installation used during developement?",
             name: "toolsUsed"
         },
         {
+            type: "input",
             message: "How to contribute?",
             name: "contribution"
-        },
-
-    ]).then(response => {
-        console.log(response);
-        const projectName = `
-        <h1>${response.projectName}</h1>`;
-
-        const projectNameAppend = document.getElementById("Readme");
-        projectNameAppend.append = projectName;
-    })
-// ]).then(function({ username }) {
-//     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-//     axios
-//     .get(queryUrl)
-//     .then(data => {
-//       // console.log(result.data)
-//      var userProfilePic = data.owner.avatar_url
-
-//       console.log(userProfilePic)
-
-//   const repoNameStr = repoName.join("\n")
-
-//   fs.writeFile("gitHubRepo.json", repoNameStr, err => {
-//     if(err) {
-//       throw err
-//     }
-//   })
-// })
+        }
+    ]);
+    }
 
 // const questions = [
 
 
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
 
-function init() {
-
-}
-
-init();
+function generateMarkdown(data) {
+    return `
+      # ${data.projectName}
+      
+      ## About the project
+      
+      ${data.projectDescription}
+      ---
+      
+      ## Getting started
+      Below are the prerequisite understanding and programs that were utilized :
+      * ${data.toolsUsed[0]}-click [here]() to a tutorial to install
+      * ${data.toolsUsed[1]}-click [here]()
+      * ${data.toolsUsed[2]}()
+      ---
+      
+      
+      ## How to contribute
+      
+      
+      ${data.contribution}
+      
+      ## Deployed link
+      
+      [Live site]${data.liveSite}
+      ---
+      
+      ## Author
+      
+      **${data.username}**
+      - [Link to Github](https://github.com/mtbanh)
+      - [Link to LinkedIn](https://www.linkedin.com/in/mai-banh-311ba6164/)
+      
+      See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+      
+      ## Acknowledgments
+      Thank you to my tutor, Matthew Chen.
+      
+      `;
+    }
+    
+    
